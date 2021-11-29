e39eb5833ff1ab9ec9daedab4ce5fbcef0a526c9
us5tt <serg.manko80@gmail.com>
2021-11-29 22:53:24 +0200

    mod orders, models myshop add celery for asinhr

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
+    'orders.apps.OrdersConfig',
 ]
 
 MIDDLEWARE = [
@@ -133,6 +134,8 @@ os.path.join(BASE_DIR, 'static_dev/')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 CART_SESSION_ID = 'cart'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
 # Default primary key field type
